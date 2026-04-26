ce691d35d1c2ef21069f2ea0e6f055c2f10b6aad
Teresa Mercader <teresam0510@gmail.com>
2026-04-26 16:35:44 +0200

    correciones

---
+++ Ejercicios/t2e4correoelectronico.py
@@ -0,0 +1,15 @@
+#Introduce una direccion de correo electronico
+correo_electronico = input ("¿cual es tu correo?")
+
+longitud_correo= len(correo_electronico)
+
+mayusculas = correo_electronico.upper()
+
+minisculas = correo_electronico.lower()
+
+print (longitud_correo)
+print (mayusculas)
+print (minisculas)
+
+
+
